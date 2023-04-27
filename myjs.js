ac42f4bb57444b95d25153f79f7207e671661234
Sagar <sagar@debian>
2023-04-27 17:54:54 +0545

    Initial commit

---
+++ myjs.js
@@ -0,0 +1,7 @@
+var logo= document.getElementsById("imgg");
+logo.addEventListener("click",function(){
+    alert("ScreenScrolled");
+});
+alert("Hello there");
+
+document.getElementById("imgg").addEventListener("click", function(){ alert("Hello World!"); });
